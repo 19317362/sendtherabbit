f14b6421c02f4266ad979b5ad7dc17de8de71918
Romanizer <romanhayn99@gmail.com>
2019-12-21 16:44:35 +0100

    first commit

--- app/src/main/java/com/hayn/sendtherabbit/MainActivity.java
+++ app/src/main/java/com/hayn/sendtherabbit/MainActivity.java
@@ -1,14 +1,19 @@
 package com.hayn.sendtherabbit;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import com.rabbitmq.*;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+
 }
