25860b802d51a6495778b7a9947c02394dc36f3d
Romanizer <romanhayn99@gmail.com>
2019-12-21 22:06:44 +0100

    empty text recognition?

--- app/src/main/java/com/hayn/sendtherabbit/MainActivity.java
+++ app/src/main/java/com/hayn/sendtherabbit/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Lifecycle;
 import androidx.lifecycle.OnLifecycleEvent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     EditText editHost;
     EditText editMessage;
     Button buttonSend;
+    TextWatcher tw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +32,29 @@ public class MainActivity extends AppCompatActivity {
         editMessage = findViewById(R.id.editMessage);
         buttonSend = findViewById(R.id.buttonSend);
 
+        //set not clickable, because app starts with no text entered in fields
+        buttonSend.setClickable(false);
+
+        //TextWatcher to enable or disable send button
+        tw = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(isEmpty(editHost) | isEmpty(editMessage)){
+                    buttonSend.setClickable(false);
+                }else{
+                    buttonSend.setClickable(true);
+                }
+            }
+        };
+
         //setup onClickListener for send button
         buttonSend.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -39,10 +65,17 @@ public class MainActivity extends AppCompatActivity {
                 createToast("Message Sent!", Toast.LENGTH_SHORT);
             }
         });
+
+        editHost.addTextChangedListener(tw);
+        editMessage.addTextChangedListener(tw);
     }
 
     private synchronized void publishMessage(){ //publishes current editText's text to host address
 
+    }
+
+    private boolean isEmpty(EditText etText){ //returns true if not empty
+        return etText.getText().toString().trim().length() == 0;
     }
 
     public void createToast(CharSequence msg, int length){
@@ -52,7 +85,7 @@ public class MainActivity extends AppCompatActivity {
     //is this needed??
     @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
     protected void onDestroy(){
-        //createToast("exiting...",Toast.LENGTH_SHORT); //possible to do
         super.onDestroy();
+        //createToast("exiting...",Toast.LENGTH_SHORT); //possible to do
     }
 }
